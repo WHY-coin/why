6fb62904285e4fadbe4d20a5eb10c43a8202132e
Ethosa <social.ethosa@gmail.com>
2025-03-07 21:26:43 +0700

    feat: upgrade sh file

--- console/sh/why.sh
+++ console/sh/why.sh
@@ -1,14 +1,2 @@
 #!/bin/bash
-
-# Проверяем наличие команд
-if command -v zenity &> /dev/null; then
-    zenity --warning --text="why" --title="Warning"
-elif command -v notify-send &> /dev/null; then
-    notify-send "Warning" "why"
-elif command -v xmessage &> /dev/null; then
-    xmessage -center "why"
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-    osascript -e 'tell app "System Events" to display dialog "why" with title "Warning" buttons {"OK"} with icon caution'
-else
-    echo "why"
-fi
+echo "why"
